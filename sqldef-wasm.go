30aaad47f9e0c6679c5ceec4f6d0c48a41d1196c
David Konsumer <konsumer@jetboystudio.com>
2019-11-05 03:32:32 -0800

    code format

--- sqldef-wasm.go
+++ sqldef-wasm.go
@@ -3,36 +3,35 @@
 package main
  
 import (
-	"strings"
-	"syscall/js"
-	"github.com/k0kubun/sqldef/schema"
+  "strings"
+  "syscall/js"
+  "github.com/k0kubun/sqldef/schema"
 )
 
 func diff(this js.Value, args []js.Value) interface {} {
-	mode := args[0].String()
-	desiredDDLs := args[1].String()
-	currentDDLs := args[2].String()
-	callback := args[3]
-	generatorMode := schema.GeneratorModeMysql
-	if (mode == "postgres"){
-		generatorMode = schema.GeneratorModePostgres
-	}
-	ddls, err := schema.GenerateIdempotentDDLs(generatorMode, desiredDDLs, currentDDLs)
-	out := strings.Join(ddls, ";\n")
-
-	// TODO: Need to figure out how to pass error in callback
-	if err != nil {
-		callback.Invoke(err.Error(), out)
-		return false
-	} else {
-		callback.Invoke(js.Null(), out)
-		return true
-	}
+  mode := args[0].String()
+  desiredDDLs := args[1].String()
+  currentDDLs := args[2].String()
+  callback := args[3]
+  generatorMode := schema.GeneratorModeMysql
+  if (mode == "postgres"){
+    generatorMode = schema.GeneratorModePostgres
+  }
+  ddls, err := schema.GenerateIdempotentDDLs(generatorMode, desiredDDLs, currentDDLs)
+  out := strings.Join(ddls, ";\n")
+  
+  if err != nil {
+    callback.Invoke(err.Error(), out)
+    return false
+  } else {
+    callback.Invoke(js.Null(), out)
+    return true
+  }
 }
  
 func main() {
-	c := make(chan bool)
-	// I wish this wasn't global!
-	js.Global().Set("_SQLDEF", js.FuncOf(diff))
-	<-c
+  c := make(chan bool)
+  // I wish this wasn't global!
+  js.Global().Set("_SQLDEF", js.FuncOf(diff))
+  <-c
 }
